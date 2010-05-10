4fc5a186c446c0af98a125df0ec3f5b312916e98
erincatto <erincatto@cafe418c-1137-0410-84cc-357ec87a5d9b>
2010-05-10 08:38:18 +0000

    - Improved contact ids for warm starting. - Edge shape work in progress. - This version might not be stable. - Fixed some typos in the manual.
    Former-commit-id: 376d4d2ab1518e825f4a14ea460275e4999b286d

--- trunk/Box2D/Box2D/Collision/b2Collision.cpp
+++ trunk/Box2D/Box2D/Collision/b2Collision.cpp
@@ -196,7 +196,7 @@ bool b2AABB::RayCast(b2RayCastOutput* output, const b2RayCastInput& input) const
 
 // Sutherland-Hodgman clipping.
 int32 b2ClipSegmentToLine(b2ClipVertex vOut[2], const b2ClipVertex vIn[2],
-						const b2Vec2& normal, float32 offset)
+						const b2Vec2& normal, float32 offset, int32 vertexIndexA)
 {
 	// Start with no output points
 	int32 numOut = 0;
@@ -215,14 +215,12 @@ int32 b2ClipSegmentToLine(b2ClipVertex vOut[2], const b2ClipVertex vIn[2],
 		// Find intersection point of edge and plane
 		float32 interp = distance0 / (distance0 - distance1);
 		vOut[numOut].v = vIn[0].v + interp * (vIn[1].v - vIn[0].v);
-		if (distance0 > 0.0f)
-		{
-			vOut[numOut].id = vIn[0].id;
-		}
-		else
-		{
-			vOut[numOut].id = vIn[1].id;
-		}
+
+		// VertexA is hitting edgeB.
+		vOut[numOut].id.cf.indexA = vertexIndexA;
+		vOut[numOut].id.cf.indexB = vIn[0].id.cf.indexB;
+		vOut[numOut].id.cf.typeA = b2ContactFeature::e_vertex;
+		vOut[numOut].id.cf.typeB = b2ContactFeature::e_edge;
 		++numOut;
 	}
 
