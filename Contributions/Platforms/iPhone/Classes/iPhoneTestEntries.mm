dc44a8d40dcf07fb47bab0f3cbc6ca16ca304e9f
erincatto <erincatto@cafe418c-1137-0410-84cc-357ec87a5d9b>
2011-03-28 06:41:30 +0000

    - Updated iPhone testbed Contribution. Thanks sakamoto!
    Former-commit-id: eeb5f82a2282fa098b7a0f2a39c94ae79f0c18f2 [formerly dbc5ad5286c0c3a75948df700b12e8f7891bd3b4]
    Former-commit-id: 7956f1baf064fa6b06ac83fe620e89795bc36128

--- Contributions/Platforms/iPhone/Classes/iPhoneTestEntries.mm
+++ Contributions/Platforms/iPhone/Classes/iPhoneTestEntries.mm
@@ -1,31 +1,35 @@
 /*
- * Copyright (c) 2006-2007 Erin Catto http://www.gphysics.com
- *
- * This software is provided 'as-is', without any express or implied
- * warranty.  In no event will the authors be held liable for any damages
- * arising from the use of this software.
- * Permission is granted to anyone to use this software for any purpose,
- * including commercial applications, and to alter it and redistribute it
- * freely, subject to the following restrictions:
- * 1. The origin of this software must not be misrepresented; you must not
- * claim that you wrote the original software. If you use this software
- * in a product, an acknowledgment in the product documentation would be
- * appreciated but is not required.
- * 2. Altered source versions must be plainly marked as such, and must not be
- * misrepresented as being the original software.
- * 3. This notice may not be removed or altered from any source distribution.
- */
+* Copyright (c) 2006-2009 Erin Catto http://www.box2d.org
+*
+* This software is provided 'as-is', without any express or implied
+* warranty.  In no event will the authors be held liable for any damages
+* arising from the use of this software.
+* Permission is granted to anyone to use this software for any purpose,
+* including commercial applications, and to alter it and redistribute it
+* freely, subject to the following restrictions:
+* 1. The origin of this software must not be misrepresented; you must not
+* claim that you wrote the original software. If you use this software
+* in a product, an acknowledgment in the product documentation would be
+* appreciated but is not required.
+* 2. Altered source versions must be plainly marked as such, and must not be
+* misrepresented as being the original software.
+* 3. This notice may not be removed or altered from any source distribution.
+*/
 
 #include "iPhoneTest.h"
-//#include "GLES-Render.h"
+#include <cstring>
+using namespace std;
 
 #include "ApplyForce.h"
 #include "BodyTypes.h"
-
 #include "Breakable.h"
 #include "Bridge.h"
-#include "CCDTest.h"
+#include "BulletTest.h"
+#include "Cantilever.h"
+#include "Car.h"
+#include "ContinuousTest.h"
 #include "Chain.h"
+#include "CharacterCollision.h"
 #include "CollisionFiltering.h"
 #include "CollisionProcessing.h"
 #include "CompoundShapes.h"
@@ -33,9 +37,11 @@
 #include "DistanceTest.h"
 #include "Dominos.h"
 #include "DynamicTreeTest.h"
+#include "EdgeShapes.h"
+#include "EdgeTest.h"
 #include "Gears.h"
-#include "LineJoint.h"
 #include "OneSidedPlatform.h"
+#include "Pinball.h"
 #include "PolyCollision.h"
 #include "PolyShapes.h"
 #include "Prismatic.h"
@@ -43,11 +49,14 @@
 #include "Pyramid.h"
 #include "RayCast.h"
 #include "Revolute.h"
+#include "Rope.h"
+#include "RopeJoint.h"
 #include "SensorTest.h"
 #include "ShapeEditing.h"
 #include "SliderCrank.h"
 #include "SphereStack.h"
 #include "TheoJansen.h"
+#include "Tiles.h"
 #include "TimeOfImpact.h"
 #include "VaryingFriction.h"
 #include "VaryingRestitution.h"
@@ -56,39 +65,47 @@
 
 TestEntry g_testEntries[] =
 {
-{"Body Types", BodyTypes::Create},
-{"CCD Test", CCDTest::Create},
-{"SphereStack", SphereStack::Create},
-{"Vertical Stack", VerticalStack::Create},
-{"Confined", Confined::Create},
-{"Bridge", Bridge::Create},
-{"Breakable", Breakable::Create},
-{"Varying Restitution", VaryingRestitution::Create},
-{"Ray-Cast", RayCast::Create},
-{"Pyramid", Pyramid::Create},
-{"PolyCollision", PolyCollision::Create},
-{"One-Sided Platform", OneSidedPlatform::Create},
-{"Apply Force", ApplyForce::Create},
-{"Chain", Chain::Create},
-{"Collision Filtering", CollisionFiltering::Create},
-{"Collision Processing", CollisionProcessing::Create},
-{"Compound Shapes", CompoundShapes::Create},
-{"Distance Test", DistanceTest::Create},
-{"Dominos", Dominos::Create},
-{"Dynamic Tree", DynamicTreeTest::Create},
-{"Gears", Gears::Create},
-{"Line Joint", LineJoint::Create},
-{"Polygon Shapes", PolyShapes::Create},
-{"Prismatic", Prismatic::Create},
-{"Pulleys", Pulleys::Create},
-{"Revolute", Revolute::Create},
-{"Sensor Test", SensorTest::Create},
-{"Shape Editing", ShapeEditing::Create},
-{"Slider Crank", SliderCrank::Create},
-{"Theo Jansen's Walker", TheoJansen::Create},
-{"Time of Impact", TimeOfImpact::Create},
-{"Varying Friction", VaryingFriction::Create},
-{"Web", Web::Create},
-{NULL, NULL}
-
+	{"Pulleys", Pulleys::Create},
+	{"SphereStack", SphereStack::Create},
+	{"Tiles", Tiles::Create},
+	{"Polygon Shapes", PolyShapes::Create},
+	{"Rope", Rope::Create},
+	{"Web", Web::Create},
+	{"Car", Car::Create},
+	{"Vertical Stack", VerticalStack::Create},
+	{"RopeJoint", RopeJoint::Create},
+	{"Character Collision", CharacterCollision::Create},
+	{"Edge Test", EdgeTest::Create},
+	{"One-Sided Platform", OneSidedPlatform::Create},
+	{"Pinball", Pinball::Create},
+	{"Bullet Test", BulletTest::Create},
+	{"Continuous Test", ContinuousTest::Create},
+	{"Time of Impact", TimeOfImpact::Create},
+	{"Ray-Cast", RayCast::Create},
+	{"Confined", Confined::Create},
+	{"Pyramid", Pyramid::Create},
+	{"Varying Restitution", VaryingRestitution::Create},
+	{"Theo Jansen's Walker", TheoJansen::Create},
+	{"Body Types", BodyTypes::Create},
+	{"Prismatic", Prismatic::Create},
+	{"Edge Shapes", EdgeShapes::Create},
+	{"PolyCollision", PolyCollision::Create},
+	{"Apply Force", ApplyForce::Create},
+	{"Cantilever", Cantilever::Create},
+	{"Bridge", Bridge::Create},
+	{"Breakable", Breakable::Create},
+	{"Chain", Chain::Create},
+	{"Collision Filtering", CollisionFiltering::Create},
+	{"Collision Processing", CollisionProcessing::Create},
+	{"Compound Shapes", CompoundShapes::Create},
+	{"Distance Test", DistanceTest::Create},
+	{"Dominos", Dominos::Create},
+	{"Dynamic Tree", DynamicTreeTest::Create},
+	{"Gears", Gears::Create},
+	{"Revolute", Revolute::Create},
+	{"Sensor Test", SensorTest::Create},
+	{"Shape Editing", ShapeEditing::Create},
+	{"Slider Crank", SliderCrank::Create},
+	{"Varying Friction", VaryingFriction::Create},
+	{NULL, NULL}
 };
