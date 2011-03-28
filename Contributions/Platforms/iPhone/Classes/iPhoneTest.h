ed3b466e9e91b04ac079ccb50f3fe31441e9a965
erincatto <erincatto@cafe418c-1137-0410-84cc-357ec87a5d9b>
2011-03-28 06:41:30 +0000

    - Updated iPhone testbed Contribution. Thanks sakamoto!
    Former-commit-id: b122370e4debcf70e6cbb79c1b02dacab86f4fed

--- Contributions/Platforms/iPhone/Classes/iPhoneTest.h
+++ Contributions/Platforms/iPhone/Classes/iPhoneTest.h
@@ -58,25 +58,28 @@ inline float32 RandomFloat(float32 lo, float32 hi)
 struct Settings
 {
 	Settings() :
-	hz(60.0f),
-	velocityIterations(10),
-	positionIterations(4),
-	drawStats(0),
-	drawShapes(1),
-	drawJoints(1),
-	drawAABBs(0),
-	drawPairs(0),
-	drawContactPoints(0),
-	drawContactNormals(0),
-	drawContactForces(0),
-	drawFrictionForces(0),
-	drawCOMs(0),
-	enableWarmStarting(1),
-	enableContinuous(1),
-	pause(0),
-	singleStep(0)
-	{}
-	
+		viewCenter(0.0f, 20.0f),
+		hz(60.0f),
+		velocityIterations(8),
+		positionIterations(3),
+		drawShapes(1),
+		drawJoints(1),
+		drawAABBs(0),
+		drawPairs(0),
+		drawContactPoints(0),
+		drawContactNormals(0),
+		drawContactForces(0),
+		drawFrictionForces(0),
+		drawCOMs(0),
+		drawStats(0),
+		enableWarmStarting(1),
+		enableContinuous(1),
+		enableSubStepping(0),
+		pause(0),
+		singleStep(0)
+		{}
+
+	b2Vec2 viewCenter;
 	float32 hz;
 	int32 velocityIterations;
 	int32 positionIterations;
@@ -92,6 +95,7 @@ struct Settings
 	int32 drawStats;
 	int32 enableWarmStarting;
 	int32 enableContinuous;
+	int32 enableSubStepping;
 	int32 pause;
 	int32 singleStep;
 };
