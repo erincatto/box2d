1bfdfe4b9b6e2c186e33caad1b5c3f443aa5041a
erincatto@gmail.com <erincatto@gmail.com@cafe418c-1137-0410-84cc-357ec87a5d9b>
2011-06-18 08:05:55 +0000

    - Made a lot of changes to the solver to make it more cache friendly. Still some work left on this. - The gear joint now allows the child joints to be connected to dynamic bodies (now a 4-way constraint). - Rotations are now stored in a more compact form.
    Former-commit-id: 29b00b2b9a879027e00093b823fd0fe602ef1719

--- Box2D/Box2D/Dynamics/b2Island.h
+++ Box2D/Box2D/Dynamics/b2Island.h
@@ -27,21 +27,8 @@ class b2Contact;
 class b2Joint;
 class b2StackAllocator;
 class b2ContactListener;
-struct b2ContactConstraint;
-
-/// This is an internal structure.
-struct b2Position
-{
-	b2Vec2 x;
-	float32 a;
-};
-
-/// This is an internal structure.
-struct b2Velocity
-{
-	b2Vec2 v;
-	float32 w;
-};
+struct b2ContactVelocityConstraint;
+struct b2Profile;
 
 /// This is an internal class.
 class b2Island
@@ -58,15 +45,16 @@ public:
 		m_jointCount = 0;
 	}
 
-	void Solve(const b2TimeStep& step, const b2Vec2& gravity, bool allowSleep);
+	void Solve(b2Profile* profile, const b2TimeStep& step, const b2Vec2& gravity, bool allowSleep);
 
-	void SolveTOI(const b2TimeStep& subStep, const b2Body* bodyA, const b2Body* bodyB);
+	void SolveTOI(const b2TimeStep& subStep, int32 toiIndexA, int32 toiIndexB);
 
 	void Add(b2Body* body)
 	{
 		b2Assert(m_bodyCount < m_bodyCapacity);
 		body->m_islandIndex = m_bodyCount;
-		m_bodies[m_bodyCount++] = body;
+		m_bodies[m_bodyCount] = body;
+		++m_bodyCount;
 	}
 
 	void Add(b2Contact* contact)
@@ -81,7 +69,7 @@ public:
 		m_joints[m_jointCount++] = joint;
 	}
 
-	void Report(const b2ContactConstraint* constraints);
+	void Report(const b2ContactVelocityConstraint* constraints);
 
 	b2StackAllocator* m_allocator;
 	b2ContactListener* m_listener;
