b66c9cb969e41e99a69184e166bd6c0646cf11c4
erincatto@gmail.com <erincatto@gmail.com@cafe418c-1137-0410-84cc-357ec87a5d9b>
2011-06-26 23:20:43 +0000

    Fixed issue 210. b2Timer port to Linux and Apple.
    Former-commit-id: 9f2c137370e4c694424c841820a5c7498869efef

--- Box2D/Box2D/Common/b2Timer.cpp
+++ Box2D/Box2D/Common/b2Timer.cpp
@@ -58,6 +58,30 @@ float32 b2Timer::GetMilliseconds() const
 	return ms;
 }
 
+#elif defined(__linux__) || defined (__APPLE__)
+
+#include <sys/time.h>
+
+b2Timer::b2Timer()
+{
+    Reset();
+}
+
+void b2Timer::Reset()
+{
+    timeval t;
+    gettimeofday(&t, 0);
+    m_start_sec = t.tv_sec;
+    m_start_msec = t.tv_usec * 0.001f;
+}
+
+float32 b2Timer::GetMilliseconds() const
+{
+    timeval t;
+    gettimeofday(&t, 0);
+    return (t.tv_sec - m_start_sec) * 1000 + t.tv_usec * 0.001f - m_start_msec;
+}
+
 #else
 
 b2Timer::b2Timer()
--- Box2D/Box2D/Common/b2Timer.h
+++ Box2D/Box2D/Common/b2Timer.h
@@ -35,6 +35,11 @@ public:
 
 private:
 
+#if defined(WIN32)
 	float64 m_start;
 	static float64 s_invFrequency;
+#elif defined(__linux__) || defined (__APPLE__)
+	unsigned long m_start_sec;
+	unsigned long m_start_msec;
+#endif
 };
