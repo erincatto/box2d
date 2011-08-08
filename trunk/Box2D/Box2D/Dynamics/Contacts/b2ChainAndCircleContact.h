2bda672459b0cf05680d602c7e30422a8bdc5691
erincatto@gmail.com <erincatto@gmail.com@cafe418c-1137-0410-84cc-357ec87a5d9b>
2011-08-08 07:03:57 +0000

    b2LoopShape has become b2ChainShape. b2ChainShape handles loops and connected chains.
    Former-commit-id: 823b3f95b1e8616edeab7d5c8cebe8b2d6be5e4e

--- trunk/Box2D/Box2D/Dynamics/Contacts/b2ChainAndCircleContact.h
+++ trunk/Box2D/Box2D/Dynamics/Contacts/b2ChainAndCircleContact.h
@@ -16,22 +16,22 @@
 * 3. This notice may not be removed or altered from any source distribution.
 */
 
-#ifndef B2_LOOP_AND_CIRCLE_CONTACT_H
-#define B2_LOOP_AND_CIRCLE_CONTACT_H
+#ifndef B2_CHAIN_AND_CIRCLE_CONTACT_H
+#define B2_CHAIN_AND_CIRCLE_CONTACT_H
 
 #include <Box2D/Dynamics/Contacts/b2Contact.h>
 
 class b2BlockAllocator;
 
-class b2LoopAndCircleContact : public b2Contact
+class b2ChainAndCircleContact : public b2Contact
 {
 public:
 	static b2Contact* Create(	b2Fixture* fixtureA, int32 indexA,
 								b2Fixture* fixtureB, int32 indexB, b2BlockAllocator* allocator);
 	static void Destroy(b2Contact* contact, b2BlockAllocator* allocator);
 
-	b2LoopAndCircleContact(b2Fixture* fixtureA, int32 indexA, b2Fixture* fixtureB, int32 indexB);
-	~b2LoopAndCircleContact() {}
+	b2ChainAndCircleContact(b2Fixture* fixtureA, int32 indexA, b2Fixture* fixtureB, int32 indexB);
+	~b2ChainAndCircleContact() {}
 
 	void Evaluate(b2Manifold* manifold, const b2Transform& xfA, const b2Transform& xfB);
 };
