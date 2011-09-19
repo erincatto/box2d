2eb9f1ce36a1acb0e4b22822fc979f733fdc8a8e
erincatto@gmail.com <erincatto@gmail.com@cafe418c-1137-0410-84cc-357ec87a5d9b>
2011-09-19 05:49:06 +0000

    Implemented a convex hull algorithm for b2PolygonShape. The removes the winding order and convexity requirements from the input point array.
    Former-commit-id: ba3e35b54cacfa37ab064c95400cbbe45106e6b8

--- Box2D/Testbed/Framework/Render.h
+++ Box2D/Testbed/Framework/Render.h
@@ -44,6 +44,8 @@ public:
 
     void DrawString(int x, int y, const char* string, ...); 
 
+    void DrawString(const b2Vec2& p, const char* string, ...);
+
     void DrawAABB(b2AABB* aabb, const b2Color& color);
 };
 
