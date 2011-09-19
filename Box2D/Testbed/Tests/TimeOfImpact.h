0715e8405858b5ae626f6394028e712b5d382267
erincatto@gmail.com <erincatto@gmail.com@cafe418c-1137-0410-84cc-357ec87a5d9b>
2011-09-19 05:49:06 +0000

    Implemented a convex hull algorithm for b2PolygonShape. The removes the winding order and convexity requirements from the input point array.
    Former-commit-id: 0e007add6207c82820996019e66c204aee7efcb0 [formerly ba3e35b54cacfa37ab064c95400cbbe45106e6b8]
    Former-commit-id: 2eb9f1ce36a1acb0e4b22822fc979f733fdc8a8e

--- Box2D/Testbed/Tests/TimeOfImpact.h
+++ Box2D/Testbed/Tests/TimeOfImpact.h
@@ -76,11 +76,11 @@ public:
 
 		b2Transform transformA;
 		sweepA.GetTransform(&transformA, 0.0f);
-		for (int32 i = 0; i < m_shapeA.m_vertexCount; ++i)
+		for (int32 i = 0; i < m_shapeA.m_count; ++i)
 		{
 			vertices[i] = b2Mul(transformA, m_shapeA.m_vertices[i]);
 		}
-		m_debugDraw.DrawPolygon(vertices, m_shapeA.m_vertexCount, b2Color(0.9f, 0.9f, 0.9f));
+		m_debugDraw.DrawPolygon(vertices, m_shapeA.m_count, b2Color(0.9f, 0.9f, 0.9f));
 
 		b2Transform transformB;
 		sweepB.GetTransform(&transformB, 0.0f);
@@ -91,35 +91,35 @@ public:
 		b2Vec2 vB = sweepB.c - sweepB.c0;
 		b2Vec2 v = vB + b2Cross(wB, rB);
 
-		for (int32 i = 0; i < m_shapeB.m_vertexCount; ++i)
+		for (int32 i = 0; i < m_shapeB.m_count; ++i)
 		{
 			vertices[i] = b2Mul(transformB, m_shapeB.m_vertices[i]);
 		}
-		m_debugDraw.DrawPolygon(vertices, m_shapeB.m_vertexCount, b2Color(0.5f, 0.9f, 0.5f));
+		m_debugDraw.DrawPolygon(vertices, m_shapeB.m_count, b2Color(0.5f, 0.9f, 0.5f));
 
 		sweepB.GetTransform(&transformB, output.t);
-		for (int32 i = 0; i < m_shapeB.m_vertexCount; ++i)
+		for (int32 i = 0; i < m_shapeB.m_count; ++i)
 		{
 			vertices[i] = b2Mul(transformB, m_shapeB.m_vertices[i]);
 		}
-		m_debugDraw.DrawPolygon(vertices, m_shapeB.m_vertexCount, b2Color(0.5f, 0.7f, 0.9f));
+		m_debugDraw.DrawPolygon(vertices, m_shapeB.m_count, b2Color(0.5f, 0.7f, 0.9f));
 
 		sweepB.GetTransform(&transformB, 1.0f);
-		for (int32 i = 0; i < m_shapeB.m_vertexCount; ++i)
+		for (int32 i = 0; i < m_shapeB.m_count; ++i)
 		{
 			vertices[i] = b2Mul(transformB, m_shapeB.m_vertices[i]);
 		}
-		m_debugDraw.DrawPolygon(vertices, m_shapeB.m_vertexCount, b2Color(0.9f, 0.5f, 0.5f));
+		m_debugDraw.DrawPolygon(vertices, m_shapeB.m_count, b2Color(0.9f, 0.5f, 0.5f));
 
 #if 0
 		for (float32 t = 0.0f; t < 1.0f; t += 0.1f)
 		{
 			sweepB.GetTransform(&transformB, t);
-			for (int32 i = 0; i < m_shapeB.m_vertexCount; ++i)
+			for (int32 i = 0; i < m_shapeB.m_count; ++i)
 			{
 				vertices[i] = b2Mul(transformB, m_shapeB.m_vertices[i]);
 			}
-			m_debugDraw.DrawPolygon(vertices, m_shapeB.m_vertexCount, b2Color(0.9f, 0.5f, 0.5f));
+			m_debugDraw.DrawPolygon(vertices, m_shapeB.m_count, b2Color(0.9f, 0.5f, 0.5f));
 		}
 #endif
 	}
