42ea9ac6fbcdcd4f0662fd815b51045a278c97be
erincatto@gmail.com <erincatto@gmail.com@cafe418c-1137-0410-84cc-357ec87a5d9b>
2011-11-27 00:46:35 +0000

    Added header guards to b2Draw.h and b2Timer.h. Cleaned up testbed controls.
    Former-commit-id: 81452e09539bc62e0ea16ef16113b5d8d4cfc2c3

--- trunk/Box2D/Box2D/Common/b2Timer.h
+++ trunk/Box2D/Box2D/Common/b2Timer.h
@@ -16,6 +16,9 @@
 * 3. This notice may not be removed or altered from any source distribution.
 */
 
+#ifndef B2_TIMER_H
+#define B2_TIMER_H
+
 #include <Box2D/Common/b2Settings.h>
 
 /// Timer for profiling. This has platform specific code and may
@@ -43,3 +46,5 @@ private:
 	unsigned long m_start_msec;
 #endif
 };
+
+#endif
