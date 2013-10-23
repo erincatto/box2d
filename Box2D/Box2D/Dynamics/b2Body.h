4bffff6bf460b39380f7704b7f15d2cc6ba0e920
erincatto@gmail.com <erincatto@gmail.com@cafe418c-1137-0410-84cc-357ec87a5d9b>
2013-10-23 18:48:50 +0000

    Fix for issue 290.
    Former-commit-id: 3232c8d53efacb4125c0df7c6e7fafdeecb8bfeb

--- Box2D/Box2D/Dynamics/b2Body.h
+++ Box2D/Box2D/Dynamics/b2Body.h
@@ -154,8 +154,8 @@ public:
 	void DestroyFixture(b2Fixture* fixture);
 
 	/// Set the position of the body's origin and rotation.
-	/// This breaks any contacts and wakes the other bodies.
 	/// Manipulating a body's transform may cause non-physical behavior.
+	/// Note: contacts are updated on the next call to b2World::Step.
 	/// @param position the world position of the body's local origin.
 	/// @param angle the world rotation in radians.
 	void SetTransform(const b2Vec2& position, float32 angle);
