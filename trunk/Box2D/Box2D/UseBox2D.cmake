17295ea5ffdb0fcd1c96e1c87ec0757662c4966c
erincatto@gmail.com <erincatto@gmail.com@cafe418c-1137-0410-84cc-357ec87a5d9b>
2013-11-03 18:32:32 +0000

    CMake Linux changes for issue 320.
    Former-commit-id: 898d20111ce0939ca6c83bfb98c0efd32325453a

---
+++ trunk/Box2D/Box2D/UseBox2D.cmake
@@ -0,0 +1,9 @@
+#                                               -*- cmake -*-
+#
+#  UseBox2d.cmake
+#
+
+add_definitions     ( ${BOX2D_DEFINITIONS} )
+include_directories ( ${BOX2D_INCLUDE_DIRS} )
+link_directories    ( ${BOX2D_LIBRARY_DIRS} )
+
