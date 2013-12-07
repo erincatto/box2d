3187a3e78158fcd4bca7311bd67c18287cab592b
erincatto@gmail.com <erincatto@gmail.com@cafe418c-1137-0410-84cc-357ec87a5d9b>
2013-12-07 06:26:32 +0000

    Fixed xcode5 build.
    Former-commit-id: 2a6a26e50688f14409b61f3494d26bd9f8ac0c52

--- Box2D/Testbed/Framework/RenderGL3.cpp
+++ Box2D/Testbed/Framework/RenderGL3.cpp
@@ -110,7 +110,7 @@ static char g_textPool[TEXT_POOL_SIZE];
 static unsigned g_textPoolSize = 0;
 static const char* allocText(const char* text)
 {
-	unsigned len = strlen(text) + 1;
+	unsigned len = (unsigned)strlen(text) + 1;
 	if (g_textPoolSize + len >= TEXT_POOL_SIZE)
 		return 0;
 	char* dst = &g_textPool[g_textPoolSize];
