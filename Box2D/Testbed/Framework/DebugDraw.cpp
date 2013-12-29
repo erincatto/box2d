a0cebde62a85877eedf5644bdf6d90e4179c7aad
erincatto@gmail.com <erincatto@gmail.com@cafe418c-1137-0410-84cc-357ec87a5d9b>
2013-12-29 08:07:50 +0000

    Implemented render batching to improve debug draw performance. Now there is a sorting issue.
    Former-commit-id: 660368144c8ebe966de2c143985d03b779fca285

--- Box2D/Testbed/Framework/DebugDraw.cpp
+++ Box2D/Testbed/Framework/DebugDraw.cpp
@@ -186,7 +186,265 @@ static GLuint sCreateShaderProgram(const char* vs, const char* fs)
 }
 
 //
-struct GLRender
+struct GLRenderPoints
+{
+	void Create()
+	{
+		const char* vs = \
+        "#version 400\n"
+        "uniform mat4 projectionMatrix;\n"
+        "layout(location = 0) in vec2 v_position;\n"
+        "layout(location = 1) in vec4 v_color;\n"
+        "out vec4 f_color;\n"
+        "void main(void)\n"
+        "{\n"
+        "	f_color = v_color;\n"
+        "	gl_Position = projectionMatrix * vec4(v_position, 0.0f, 1.0f);\n"
+        "}\n";
+        
+		const char* fs = \
+        "#version 400\n"
+        "in vec4 f_color;\n"
+        "out vec4 color;\n"
+        "void main(void)\n"
+        "{\n"
+        "	color = f_color;\n"
+        "}\n";
+        
+		m_programId = sCreateShaderProgram(vs, fs);
+		m_projectionUniform = glGetUniformLocation(m_programId, "projectionMatrix");
+		m_vertexAttribute = 0;
+		m_colorAttribute = 1;
+        
+		// Generate
+		glGenVertexArrays(1, &m_vaoId);
+		glGenBuffers(2, m_vboIds);
+        
+		glBindVertexArray(m_vaoId);
+		glEnableVertexAttribArray(m_vertexAttribute);
+		glEnableVertexAttribArray(m_colorAttribute);
+        
+		// Vertex buffer
+		glBindBuffer(GL_ARRAY_BUFFER, m_vboIds[0]);
+		glVertexAttribPointer(m_vertexAttribute, 2, GL_FLOAT, GL_FALSE, 0, BUFFER_OFFSET(0));
+		glBufferData(GL_ARRAY_BUFFER, sizeof(m_vertices), m_vertices, GL_DYNAMIC_DRAW);
+        
+		glBindBuffer(GL_ARRAY_BUFFER, m_vboIds[1]);
+		glVertexAttribPointer(m_colorAttribute, 4, GL_FLOAT, GL_FALSE, 0, BUFFER_OFFSET(0));
+		glBufferData(GL_ARRAY_BUFFER, sizeof(m_colors), m_colors, GL_DYNAMIC_DRAW);
+        
+		sCheckGLError();
+        
+		// Cleanup
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+		glBindVertexArray(0);
+        
+		m_count = 0;
+	}
+    
+	void Destroy()
+	{
+		if (m_vaoId)
+		{
+			glDeleteVertexArrays(1, &m_vaoId);
+			glDeleteBuffers(2, m_vboIds);
+			m_vaoId = 0;
+		}
+        
+		if (m_programId)
+		{
+			glDeleteProgram(m_programId);
+			m_programId = 0;
+		}
+	}
+    
+	void Vertex(const b2Vec2& v, const b2Color& c)
+	{
+		if (m_count == e_maxVertices)
+			Flush();
+        
+		m_vertices[m_count] = v;
+		m_colors[m_count] = c;
+		++m_count;
+	}
+    
+    void Flush()
+	{
+        if (m_count == 0)
+            return;
+        
+		glUseProgram(m_programId);
+        
+		float32 proj[16] = { 0.0f };
+		g_camera.BuildProjectionMatrix(proj);
+        
+		glUniformMatrix4fv(m_projectionUniform, 1, GL_FALSE, proj);
+        
+		glBindVertexArray(m_vaoId);
+        
+		glBindBuffer(GL_ARRAY_BUFFER, m_vboIds[0]);
+		glBufferSubData(GL_ARRAY_BUFFER, 0, m_count * sizeof(b2Vec2), m_vertices);
+        
+		glBindBuffer(GL_ARRAY_BUFFER, m_vboIds[1]);
+		glBufferSubData(GL_ARRAY_BUFFER, 0, m_count * sizeof(b2Color), m_colors);
+        
+        // TODO use gl_PointSize?
+        glPointSize(4.0f);
+		glDrawArrays(GL_POINTS, 0, m_count);
+        
+		sCheckGLError();
+        
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+		glBindVertexArray(0);
+		glUseProgram(0);
+        
+		m_count = 0;
+	}
+    
+	enum { e_maxVertices = 512 };
+	b2Vec2 m_vertices[e_maxVertices];
+	b2Color m_colors[e_maxVertices];
+    
+	int32 m_count;
+    
+	GLuint m_vaoId;
+	GLuint m_vboIds[2];
+	GLuint m_programId;
+	GLint m_projectionUniform;
+	GLint m_vertexAttribute;
+	GLint m_colorAttribute;
+};
+
+//
+struct GLRenderLines
+{
+	void Create()
+	{
+		const char* vs = \
+        "#version 400\n"
+        "uniform mat4 projectionMatrix;\n"
+        "layout(location = 0) in vec2 v_position;\n"
+        "layout(location = 1) in vec4 v_color;\n"
+        "out vec4 f_color;\n"
+        "void main(void)\n"
+        "{\n"
+        "	f_color = v_color;\n"
+        "	gl_Position = projectionMatrix * vec4(v_position, 0.0f, 1.0f);\n"
+        "}\n";
+        
+		const char* fs = \
+        "#version 400\n"
+        "in vec4 f_color;\n"
+        "out vec4 color;\n"
+        "void main(void)\n"
+        "{\n"
+        "	color = f_color;\n"
+        "}\n";
+        
+		m_programId = sCreateShaderProgram(vs, fs);
+		m_projectionUniform = glGetUniformLocation(m_programId, "projectionMatrix");
+		m_vertexAttribute = 0;
+		m_colorAttribute = 1;
+        
+		// Generate
+		glGenVertexArrays(1, &m_vaoId);
+		glGenBuffers(2, m_vboIds);
+        
+		glBindVertexArray(m_vaoId);
+		glEnableVertexAttribArray(m_vertexAttribute);
+		glEnableVertexAttribArray(m_colorAttribute);
+        
+		// Vertex buffer
+		glBindBuffer(GL_ARRAY_BUFFER, m_vboIds[0]);
+		glVertexAttribPointer(m_vertexAttribute, 2, GL_FLOAT, GL_FALSE, 0, BUFFER_OFFSET(0));
+		glBufferData(GL_ARRAY_BUFFER, sizeof(m_vertices), m_vertices, GL_DYNAMIC_DRAW);
+        
+		glBindBuffer(GL_ARRAY_BUFFER, m_vboIds[1]);
+		glVertexAttribPointer(m_colorAttribute, 4, GL_FLOAT, GL_FALSE, 0, BUFFER_OFFSET(0));
+		glBufferData(GL_ARRAY_BUFFER, sizeof(m_colors), m_colors, GL_DYNAMIC_DRAW);
+        
+		sCheckGLError();
+        
+		// Cleanup
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+		glBindVertexArray(0);
+        
+		m_count = 0;
+	}
+    
+	void Destroy()
+	{
+		if (m_vaoId)
+		{
+			glDeleteVertexArrays(1, &m_vaoId);
+			glDeleteBuffers(2, m_vboIds);
+			m_vaoId = 0;
+		}
+        
+		if (m_programId)
+		{
+			glDeleteProgram(m_programId);
+			m_programId = 0;
+		}
+	}
+    
+	void Vertex(const b2Vec2& v, const b2Color& c)
+	{
+		if (m_count == e_maxVertices)
+			Flush();
+        
+		m_vertices[m_count] = v;
+		m_colors[m_count] = c;
+		++m_count;
+	}
+    
+    void Flush()
+	{
+        if (m_count == 0)
+            return;
+        
+		glUseProgram(m_programId);
+        
+		float32 proj[16] = { 0.0f };
+		g_camera.BuildProjectionMatrix(proj);
+        
+		glUniformMatrix4fv(m_projectionUniform, 1, GL_FALSE, proj);
+        
+		glBindVertexArray(m_vaoId);
+        
+		glBindBuffer(GL_ARRAY_BUFFER, m_vboIds[0]);
+		glBufferSubData(GL_ARRAY_BUFFER, 0, m_count * sizeof(b2Vec2), m_vertices);
+        
+		glBindBuffer(GL_ARRAY_BUFFER, m_vboIds[1]);
+		glBufferSubData(GL_ARRAY_BUFFER, 0, m_count * sizeof(b2Color), m_colors);
+        
+		glDrawArrays(GL_LINES, 0, m_count);
+        
+		sCheckGLError();
+        
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+		glBindVertexArray(0);
+		glUseProgram(0);
+        
+		m_count = 0;
+	}
+    
+	enum { e_maxVertices = 2 * 512 };
+	b2Vec2 m_vertices[e_maxVertices];
+	b2Color m_colors[e_maxVertices];
+    
+	int32 m_count;
+    
+	GLuint m_vaoId;
+	GLuint m_vboIds[2];
+	GLuint m_programId;
+	GLint m_projectionUniform;
+	GLint m_vertexAttribute;
+	GLint m_colorAttribute;
+};
+
+//
+struct GLRenderTriangles
 {
 	void Create()
 	{
@@ -239,9 +497,7 @@ struct GLRender
 		glBindBuffer(GL_ARRAY_BUFFER, 0);
 		glBindVertexArray(0);
 
-		m_color.Set(1.0f, 1.0f, 1.0f);
 		m_count = 0;
-		m_mode = GL_NONE;
 	}
 
 	void Destroy()
@@ -260,63 +516,55 @@ struct GLRender
 		}
 	}
 
-	void Begin(GLenum mode)
-	{
-		m_count = 0;
-		m_mode = mode;
-	}
-
-	void Color(const b2Color& c)
-	{
-		m_color = c;
-	}
-
-	void Vertex(const b2Vec2& v)
+	void Vertex(const b2Vec2& v, const b2Color& c)
 	{
 		if (m_count == e_maxVertices)
-			return;
+			Flush();
 
 		m_vertices[m_count] = v;
-		m_colors[m_count] = m_color;
+		m_colors[m_count] = c;
 		++m_count;
 	}
 
-	void End()
+    void Flush()
 	{
+        if (m_count == 0)
+            return;
+        
 		glUseProgram(m_programId);
-
+        
 		float32 proj[16] = { 0.0f };
 		g_camera.BuildProjectionMatrix(proj);
-
+        
 		glUniformMatrix4fv(m_projectionUniform, 1, GL_FALSE, proj);
-
+        
 		glBindVertexArray(m_vaoId);
-
+        
 		glBindBuffer(GL_ARRAY_BUFFER, m_vboIds[0]);
 		glBufferSubData(GL_ARRAY_BUFFER, 0, m_count * sizeof(b2Vec2), m_vertices);
-
+        
 		glBindBuffer(GL_ARRAY_BUFFER, m_vboIds[1]);
 		glBufferSubData(GL_ARRAY_BUFFER, 0, m_count * sizeof(b2Color), m_colors);
-
-		glDrawArrays(m_mode, 0, m_count);
-
+        
+        glEnable(GL_BLEND);
+        glBlendFunc (GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+		glDrawArrays(GL_TRIANGLES, 0, m_count);
+        glDisable(GL_BLEND);
+        
 		sCheckGLError();
-
+        
 		glBindBuffer(GL_ARRAY_BUFFER, 0);
 		glBindVertexArray(0);
 		glUseProgram(0);
-
+        
 		m_count = 0;
-		m_mode = GL_NONE;
 	}
-
-	enum { e_maxVertices = 128 };
+    
+	enum { e_maxVertices = 3 * 512 };
 	b2Vec2 m_vertices[e_maxVertices];
 	b2Color m_colors[e_maxVertices];
 
-	GLenum m_mode;
 	int32 m_count;
-	b2Color m_color;
 
 	GLuint m_vaoId;
 	GLuint m_vboIds[2];
@@ -329,154 +577,178 @@ struct GLRender
 //
 DebugDraw::DebugDraw()
 {
-	m_render = NULL;
+	m_points = NULL;
+    m_lines = NULL;
+    m_triangles = NULL;
 }
 
 //
 DebugDraw::~DebugDraw()
 {
-	b2Assert(m_render == NULL);
+	b2Assert(m_points == NULL);
+	b2Assert(m_lines == NULL);
+	b2Assert(m_triangles == NULL);
 }
 
 //
 void DebugDraw::Create()
 {
-	m_render = new GLRender;
-	m_render->Create();
+	m_points = new GLRenderPoints;
+	m_points->Create();
+	m_lines = new GLRenderLines;
+	m_lines->Create();
+	m_triangles = new GLRenderTriangles;
+	m_triangles->Create();
 }
 
 //
 void DebugDraw::Destroy()
 {
-	m_render->Destroy();
-	delete m_render;
-	m_render = NULL;
+	m_points->Destroy();
+	delete m_points;
+	m_points = NULL;
+
+	m_lines->Destroy();
+	delete m_lines;
+	m_lines = NULL;
+
+	m_triangles->Destroy();
+	delete m_triangles;
+	m_triangles = NULL;
 }
 
 //
 void DebugDraw::DrawPolygon(const b2Vec2* vertices, int32 vertexCount, const b2Color& color)
 {
-	m_render->Begin(GL_LINE_LOOP);
-	m_render->Color(color);
+    b2Vec2 p1 = vertices[vertexCount - 1];
 	for (int32 i = 0; i < vertexCount; ++i)
 	{
-		m_render->Vertex(vertices[i]);
+        b2Vec2 p2 = vertices[i];
+		m_lines->Vertex(p1, color);
+		m_lines->Vertex(p2, color);
+        p1 = p2;
 	}
-	m_render->End();
 }
 
+//
 void DebugDraw::DrawSolidPolygon(const b2Vec2* vertices, int32 vertexCount, const b2Color& color)
 {
 	b2Color fillColor(0.5f * color.r, 0.5f * color.g, 0.5f * color.b, 0.5f);
 
-	glEnable(GL_BLEND);
-	glBlendFunc (GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-	m_render->Begin(GL_TRIANGLE_FAN);
-	m_render->Color(fillColor);
-	for (int32 i = 0; i < vertexCount; ++i)
-	{
-		m_render->Vertex(vertices[i]);
-	}
-	m_render->End();
-	glDisable(GL_BLEND);
+    for (int32 i = 1; i < vertexCount - 1; ++i)
+    {
+        m_triangles->Vertex(vertices[0], fillColor);
+        m_triangles->Vertex(vertices[i], fillColor);
+        m_triangles->Vertex(vertices[i+1], fillColor);
+    }
 
-	m_render->Begin(GL_LINE_LOOP);
-	m_render->Color(color);
+    b2Vec2 p1 = vertices[vertexCount - 1];
 	for (int32 i = 0; i < vertexCount; ++i)
 	{
-		m_render->Vertex(vertices[i]);
+        b2Vec2 p2 = vertices[i];
+		m_lines->Vertex(p1, color);
+		m_lines->Vertex(p2, color);
+        p1 = p2;
 	}
-	m_render->End();
 }
 
+//
 void DebugDraw::DrawCircle(const b2Vec2& center, float32 radius, const b2Color& color)
 {
 	const float32 k_segments = 16.0f;
 	const float32 k_increment = 2.0f * b2_pi / k_segments;
-	float32 theta = 0.0f;
-	m_render->Begin(GL_LINE_LOOP);
-	m_render->Color(color);
+    float32 sinInc = sinf(k_increment);
+    float32 cosInc = cosf(k_increment);
+    b2Vec2 r1(1.0f, 0.0f);
+    b2Vec2 v1 = center + radius * r1;
 	for (int32 i = 0; i < k_segments; ++i)
 	{
-		b2Vec2 v = center + radius * b2Vec2(cosf(theta), sinf(theta));
-		m_render->Vertex(v);
-		theta += k_increment;
+        // Perform rotation to avoid additional trigonometry.
+        b2Vec2 r2;
+        r2.x = cosInc * r1.x - sinInc * r1.y;
+        r2.y = sinInc * r1.x + cosInc * r1.y;
+		b2Vec2 v2 = center + radius * r2;
+        m_lines->Vertex(v1, color);
+        m_lines->Vertex(v2, color);
+        r1 = r2;
+        v1 = v2;
 	}
-	m_render->End();
 }
 
+//
 void DebugDraw::DrawSolidCircle(const b2Vec2& center, float32 radius, const b2Vec2& axis, const b2Color& color)
 {
 	const float32 k_segments = 16.0f;
 	const float32 k_increment = 2.0f * b2_pi / k_segments;
-	float32 theta = 0.0f;
+    float32 sinInc = sinf(k_increment);
+    float32 cosInc = cosf(k_increment);
+    b2Vec2 v0 = center;
+    b2Vec2 r1(cosInc, sinInc);
+    b2Vec2 v1 = center + radius * r1;
 	b2Color fillColor(0.5f * color.r, 0.5f * color.g, 0.5f * color.b, 0.5f);
-	glEnable(GL_BLEND);
-	glBlendFunc (GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-	m_render->Begin(GL_TRIANGLE_FAN);
-	m_render->Color(fillColor);
 	for (int32 i = 0; i < k_segments; ++i)
 	{
-		b2Vec2 v = center + radius * b2Vec2(cosf(theta), sinf(theta));
-		m_render->Vertex(v);
-		theta += k_increment;
+        // Perform rotation to avoid additional trigonometry.
+        b2Vec2 r2;
+        r2.x = cosInc * r1.x - sinInc * r1.y;
+        r2.y = sinInc * r1.x + cosInc * r1.y;
+		b2Vec2 v2 = center + radius * r2;
+		m_triangles->Vertex(v0, fillColor);
+        m_triangles->Vertex(v1, fillColor);
+        m_triangles->Vertex(v2, fillColor);
+        r1 = r2;
+        v1 = v2;
 	}
-	m_render->End();
-	glDisable(GL_BLEND);
 
-	theta = 0.0f;
-	m_render->Begin(GL_LINE_LOOP);
-	m_render->Color(color);
+    r1.Set(1.0f, 0.0f);
+    v1 = center + radius * r1;
 	for (int32 i = 0; i < k_segments; ++i)
 	{
-		b2Vec2 v = center + radius * b2Vec2(cosf(theta), sinf(theta));
-		m_render->Vertex(v);
-		theta += k_increment;
+        b2Vec2 r2;
+        r2.x = cosInc * r1.x - sinInc * r1.y;
+        r2.y = sinInc * r1.x + cosInc * r1.y;
+		b2Vec2 v2 = center + radius * r2;
+        m_lines->Vertex(v1, color);
+        m_lines->Vertex(v2, color);
+        r1 = r2;
+        v1 = v2;
 	}
-	m_render->End();
 
+    // Draw a line fixed in the circle to animate rotation.
 	b2Vec2 p = center + radius * axis;
-	m_render->Begin(GL_LINES);
-	m_render->Vertex(center);
-	m_render->Vertex(p);
-	m_render->End();
+	m_lines->Vertex(center, color);
+	m_lines->Vertex(p, color);
 }
 
+//
 void DebugDraw::DrawSegment(const b2Vec2& p1, const b2Vec2& p2, const b2Color& color)
 {
-	m_render->Begin(GL_LINES);
-	m_render->Color(color);
-	m_render->Vertex(p1);
-	m_render->Vertex(p2);
-	m_render->End();
+	m_lines->Vertex(p1, color);
+	m_lines->Vertex(p2, color);
 }
 
+//
 void DebugDraw::DrawTransform(const b2Transform& xf)
 {
-	b2Vec2 p1 = xf.p, p2;
 	const float32 k_axisScale = 0.4f;
-	m_render->Begin(GL_LINES);
-	m_render->Color(b2Color(1.0f, 0.0f, 0.0f));
-	m_render->Vertex(p1);
+    b2Color red(1.0f, 0.0f, 0.0f);
+    b2Color green(0.0f, 1.0f, 0.0f);
+	b2Vec2 p1 = xf.p, p2;
+
+	m_lines->Vertex(p1, red);
 	p2 = p1 + k_axisScale * xf.q.GetXAxis();
-	m_render->Vertex(p2);
+	m_lines->Vertex(p2, red);
 
-	m_render->Color(b2Color(0.0f, 1.0f, 0.0f));
-	m_render->Vertex(p1);
+	m_lines->Vertex(p1, green);
 	p2 = p1 + k_axisScale * xf.q.GetYAxis();
-	m_render->Vertex(p2);
-
-	m_render->End();
+	m_lines->Vertex(p2, green);
 }
 
 void DebugDraw::DrawPoint(const b2Vec2& p, float32 size, const b2Color& color)
 {
-	glPointSize(size);
-	m_render->Begin(GL_POINTS);
-	m_render->Color(color);
-	m_render->Vertex(p);
-	m_render->End();
-	glPointSize(1.0f);
+    // TODO_ERIN implement point size
+    B2_NOT_USED(size);
+    m_points->Vertex(p, color);
 }
 
 void DebugDraw::DrawString(int x, int y, const char *string, ...)
@@ -510,11 +782,28 @@ void DebugDraw::DrawString(const b2Vec2& pw, const char *string, ...)
 
 void DebugDraw::DrawAABB(b2AABB* aabb, const b2Color& c)
 {
-	m_render->Begin(GL_LINE_LOOP);
-	m_render->Color(c);
-	m_render->Vertex(aabb->lowerBound);
-	m_render->Vertex(b2Vec2(aabb->upperBound.x, aabb->lowerBound.y));
-	m_render->Vertex(aabb->upperBound);
-	m_render->Vertex(b2Vec2(aabb->lowerBound.x, aabb->upperBound.y));
-	m_render->End();
+    b2Vec2 p1 = aabb->lowerBound;
+    b2Vec2 p2 = b2Vec2(aabb->upperBound.x, aabb->lowerBound.y);
+    b2Vec2 p3 = aabb->upperBound;
+    b2Vec2 p4 = b2Vec2(aabb->lowerBound.x, aabb->upperBound.y);
+    
+    m_lines->Vertex(p1, c);
+    m_lines->Vertex(p2, c);
+
+    m_lines->Vertex(p2, c);
+    m_lines->Vertex(p3, c);
+
+    m_lines->Vertex(p3, c);
+    m_lines->Vertex(p4, c);
+
+    m_lines->Vertex(p4, c);
+    m_lines->Vertex(p1, c);
+}
+
+//
+void DebugDraw::Flush()
+{
+    m_triangles->Flush();
+    m_lines->Flush();
+    m_points->Flush();
 }
