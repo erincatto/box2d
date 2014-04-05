886c0706bab6d8e1296ac9189c1650af746d964a
erincatto@gmail.com <erincatto@gmail.com@cafe418c-1137-0410-84cc-357ec87a5d9b>
2014-04-05 18:40:14 +0000

    Fix for issue 327. Release warning fixed.
    Former-commit-id: b1b5125e479a4416b9b123b0dc529dafce8c356a [formerly 695fdb0ff560e6fb2669f32f0965f32bcd939b79]
    Former-commit-id: 09ba20c74b97d67e718bd127e14d8e2b46317612

--- Box2D/Box2D/Collision/Shapes/b2ChainShape.cpp
+++ Box2D/Box2D/Collision/Shapes/b2ChainShape.cpp
@@ -56,10 +56,8 @@ void b2ChainShape::CreateChain(const b2Vec2* vertices, int32 count)
 	b2Assert(count >= 2);
 	for (int32 i = 1; i < count; ++i)
 	{
-		b2Vec2 v1 = vertices[i-1];
-		b2Vec2 v2 = vertices[i];
 		// If the code crashes here, it means your vertices are too close together.
-		b2Assert(b2DistanceSquared(v1, v2) > b2_linearSlop * b2_linearSlop);
+		b2Assert(b2DistanceSquared(vertices[i-1], vertices[i]) > b2_linearSlop * b2_linearSlop);
 	}
 
 	m_count = count;
