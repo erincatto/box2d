871166508fcf2b7572ac7b3e09eb3e002f1dea0a
erincatto@gmail.com <erincatto@gmail.com@cafe418c-1137-0410-84cc-357ec87a5d9b>
2014-04-09 05:06:12 +0000

    Updated version to 2.3.2. Updated Building.txt. Premake and CMake are no longer supported. Made a weak attempt to update CMake.
    Former-commit-id: 6a38df1a2cb7f83c85b8ad5905d25427af54327d [formerly 54c0f2ff44258478851ed960f509261490e07d38]
    Former-commit-id: 43ffeab3cbe5d34fd6adf22bc6711ff4fd0158c9

--- Box2D/Box2D/Dynamics/Joints/b2WheelJoint.h
+++ Box2D/Box2D/Dynamics/Joints/b2WheelJoint.h
@@ -72,9 +72,8 @@ struct b2WheelJointDef : public b2JointDef
 };
 
 /// A wheel joint. This joint provides two degrees of freedom: translation
-/// along an axis fixed in bodyA and rotation in the plane. You can use a
-/// joint limit to restrict the range of motion and a joint motor to drive
-/// the rotation or to model rotational friction.
+/// along an axis fixed in bodyA and rotation in the plane. In other words, it is a point to
+/// line constraint with a rotational motor and a linear spring/damper.
 /// This joint is designed for vehicle suspensions.
 class b2WheelJoint : public b2Joint
 {
