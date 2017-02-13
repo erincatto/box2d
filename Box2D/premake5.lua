a2c7eb3587fae783650ae91afe85677d6e9574a3
Tom van Dijck <tom@tomvandijck.com>
2017-02-13 11:51:05 -0800

    Update premake5.lua so --help will work.

--- Box2D/premake5.lua
+++ Box2D/premake5.lua
@@ -2,7 +2,7 @@
 -- https://premake.github.io/
 
 workspace "Box2D"
-	location ( "Build/" .. _ACTION )
+	location ( "Build/%{_ACTION}" )
 	architecture "x86_64"
 	configurations { "Debug", "Release" }
 	
@@ -10,12 +10,12 @@ workspace "Box2D"
 		defines { "_CRT_SECURE_NO_WARNINGS" }	
 		
 	filter "configurations:Debug"
-		targetdir ( "Build/" .. _ACTION .. "/bin/Debug" )
+		targetdir ( "Build/%{_ACTION}/bin/Debug" )
 	 	defines { "DEBUG" }
 		symbols "On"
 
 	filter "configurations:Release"
-		targetdir ( "Build/" .. _ACTION .. "/bin/Release" )
+		targetdir ( "Build/%{_ACTION}/bin/Release" )
 		defines { "NDEBUG" }
 		optimize "On"
 
