f17f844910a7265e71274190f87a2fb0e130aed6
Erin Catto <erincatto@users.noreply.github.com>
2018-06-17 11:45:28 -0700

    Timer fix for Linux and MacOS
    PR #502

--- Box2D/Common/b2Timer.cpp
+++ Box2D/Common/b2Timer.cpp
@@ -83,7 +83,24 @@ float32 b2Timer::GetMilliseconds() const
 {
     timeval t;
     gettimeofday(&t, 0);
-    return 1000.0f * (t.tv_sec - m_start_sec) + 0.001f * (t.tv_usec - m_start_usec);
+	time_t start_sec = m_start_sec;
+	suseconds_t start_usec = m_start_usec;
+	
+	// http://www.gnu.org/software/libc/manual/html_node/Elapsed-Time.html
+	if (t.tv_usec < start_usec)
+	{
+		int nsec = (start_usec - t.tv_usec) / 1000000 + 1;
+		start_usec -= 1000000 * nsec;
+		start_sec += nsec;
+	}
+	
+	if (t.tv_usec - start_usec > 1000000)
+	{
+		int nsec = (t.tv_usec - start_usec) / 1000000;
+		start_usec += 1000000 * nsec;
+		start_sec -= nsec;
+	}
+	return 1000.0f * (t.tv_sec - start_sec) + 0.001f * (t.tv_usec - start_usec);
 }
 
 #else
