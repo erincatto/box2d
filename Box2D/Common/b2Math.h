125706c81c75df0d56643d69ef29957b80170370
akk0rd87 <amkad87@gmail.com>
2019-08-08 09:24:55 +0300

    Replace deprecated math.h with cmath
    Replace deprecated math.h with cmath and call isfinite from std namespace.

--- Box2D/Common/b2Math.h
+++ Box2D/Common/b2Math.h
@@ -20,12 +20,12 @@
 #define B2_MATH_H
 
 #include "Box2D/Common/b2Settings.h"
-#include <math.h>
+#include <cmath>
 
 /// This function is used to ensure that a floating point number is not a NaN or infinity.
 inline bool b2IsValid(float32 x)
 {
-	return isfinite(x);
+	return std::isfinite(x);
 }
 
 #define	b2Sqrt(x)	sqrtf(x)
