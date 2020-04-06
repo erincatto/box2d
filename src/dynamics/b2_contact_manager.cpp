569686bb106e596614096d234bc7a2943e0dd9f2
Erin Catto <erincatto@users.noreply.github.com>
2020-04-05 17:32:55 -0700

    Removed unnecessary body waking

--- src/dynamics/b2_contact_manager.cpp
+++ src/dynamics/b2_contact_manager.cpp
@@ -289,12 +289,5 @@ void b2ContactManager::AddPair(void* proxyUserDataA, void* proxyUserDataB)
 	}
 	bodyB->m_contactList = &c->m_nodeB;
 
-	// Wake up the bodies
-	if (fixtureA->IsSensor() == false && fixtureB->IsSensor() == false)
-	{
-		bodyA->SetAwake(true);
-		bodyB->SetAwake(true);
-	}
-
 	++m_contactCount;
 }
