a22ddee186844dd61612523259cf2fc288d03b03
Erin Catto <erincatto@users.noreply.github.com>
2020-08-03 13:37:19 -0700

    Issue 625 shape cast (#628)
    - Check for initial overlap
    - Fix initial overlap test for circles

--- testbed/tests/shape_cast.cpp
+++ testbed/tests/shape_cast.cpp
@@ -26,34 +26,62 @@
 class ShapeCast : public Test
 {
 public:
-    enum
-    {
-        e_vertexCount = 8
-    };
+	enum
+	{
+		e_vertexCount = 8
+	};
 
 	ShapeCast()
 	{
 #if 1
-        m_vAs[0].Set(-0.5f, 1.0f);
-        m_vAs[1].Set(0.5f, 1.0f);
-        m_vAs[2].Set(0.0f, 0.0f);
-        m_countA = 3;
-        m_radiusA = b2_polygonRadius;
+		m_vAs[0].Set(-0.5f, 1.0f);
+		m_vAs[1].Set(0.5f, 1.0f);
+		m_vAs[2].Set(0.0f, 0.0f);
+		m_countA = 3;
+		m_radiusA = b2_polygonRadius;
 
-        m_vBs[0].Set(-0.5f, -0.5f);
-        m_vBs[1].Set(0.5f, -0.5f);
-        m_vBs[2].Set(0.5f, 0.5f);
-        m_vBs[3].Set(-0.5f, 0.5f);
-        m_countB = 4;
-        m_radiusB = b2_polygonRadius;
+		m_vBs[0].Set(-0.5f, -0.5f);
+		m_vBs[1].Set(0.5f, -0.5f);
+		m_vBs[2].Set(0.5f, 0.5f);
+		m_vBs[3].Set(-0.5f, 0.5f);
+		m_countB = 4;
+		m_radiusB = b2_polygonRadius;
+
+		m_transformA.p.Set(0.0f, 0.25f);
+		m_transformA.q.SetIdentity();
+		m_transformB.p.Set(-4.0f, 0.0f);
+		m_transformB.q.SetIdentity();
+		m_translationB.Set(8.0f, 0.0f);
+#elif 0
+		m_vAs[0].Set(0.0f, 0.0f);
+		m_countA = 1;
+		m_radiusA = 0.5f;
+
+		m_vBs[0].Set(0.0f, 0.0f);
+		m_countB = 1;
+		m_radiusB = 0.5f;
+
+		m_transformA.p.Set(0.0f, 0.25f);
+		m_transformA.q.SetIdentity();
+		m_transformB.p.Set(-4.0f, 0.0f);
+		m_transformB.q.SetIdentity();
+		m_translationB.Set(8.0f, 0.0f);
 #else
-        m_vAs[0].Set(0.0f, 0.0f);
-        m_countA = 1;
-        m_radiusA = 0.5f;
+		m_vAs[0].Set(0.0f, 0.0f);
+		m_vAs[1].Set(2.0f, 0.0f);
+		m_countA = 2;
+		m_radiusA = b2_polygonRadius;
 
-        m_vBs[0].Set(0.0f, 0.0f);
-        m_countB = 1;
-        m_radiusB = 0.5f;
+		m_vBs[0].Set(0.0f, 0.0f);
+		m_countB = 1;
+		m_radiusB = 0.25f;
+
+		// Initial overlap
+		m_transformA.p.Set(0.0f, 0.0f);
+		m_transformA.q.SetIdentity();
+		m_transformB.p.Set(-0.244360745f, 0.05999358f);
+		m_transformB.q.SetIdentity();
+		m_translationB.Set(0.0f, 0.0399999991f);
 #endif
 	}
 
@@ -66,66 +94,79 @@ public:
 	{
 		Test::Step(settings);
 
-		b2Transform transformA;
-		transformA.p = b2Vec2(0.0f, 0.25f);
-		transformA.q.SetIdentity();
-
-		b2Transform transformB;
-		transformB.SetIdentity();
-
 		b2ShapeCastInput input;
 		input.proxyA.Set(m_vAs, m_countA, m_radiusA);
 		input.proxyB.Set(m_vBs, m_countB, m_radiusB);
-		input.transformA = transformA;
-		input.transformB = transformB;
-		input.translationB.Set(8.0f, 0.0f);
+		input.transformA = m_transformA;
+		input.transformB = m_transformB;
+		input.translationB = m_translationB;
 
 		b2ShapeCastOutput output;
-
 		bool hit = b2ShapeCast(&output, &input);
 
-        b2Transform transformB2;
-        transformB2.q = transformB.q;
-        transformB2.p = transformB.p + output.lambda * input.translationB;
-        
-        b2DistanceInput distanceInput;
-        distanceInput.proxyA.Set(m_vAs, m_countA, m_radiusA);
-        distanceInput.proxyB.Set(m_vBs, m_countB, m_radiusB);
-        distanceInput.transformA = transformA;
-        distanceInput.transformB = transformB2;
-        distanceInput.useRadii = false;
-        b2SimplexCache simplexCache;
-        simplexCache.count = 0;
-        b2DistanceOutput distanceOutput;
-        
-        b2Distance(&distanceOutput, &simplexCache, &distanceInput);
+		b2Transform transformB2;
+		transformB2.q = m_transformB.q;
+		transformB2.p = m_transformB.p + output.lambda * input.translationB;
+
+		b2DistanceInput distanceInput;
+		distanceInput.proxyA.Set(m_vAs, m_countA, m_radiusA);
+		distanceInput.proxyB.Set(m_vBs, m_countB, m_radiusB);
+		distanceInput.transformA = m_transformA;
+		distanceInput.transformB = transformB2;
+		distanceInput.useRadii = false;
+		b2SimplexCache simplexCache;
+		simplexCache.count = 0;
+		b2DistanceOutput distanceOutput;
+
+		b2Distance(&distanceOutput, &simplexCache, &distanceInput);
 
 		g_debugDraw.DrawString(5, m_textLine, "hit = %s, iters = %d, lambda = %g, distance = %g",
-            hit ? "true" : "false", output.iterations, output.lambda, distanceOutput.distance);
+			hit ? "true" : "false", output.iterations, output.lambda, distanceOutput.distance);
 		m_textLine += m_textIncrement;
 
 		b2Vec2 vertices[b2_maxPolygonVertices];
 
 		for (int32 i = 0; i < m_countA; ++i)
 		{
-			vertices[i] = b2Mul(transformA, m_vAs[i]);
+			vertices[i] = b2Mul(m_transformA, m_vAs[i]);
 		}
-        //g_debugDraw.DrawCircle(vertices[0], m_radiusA, b2Color(0.9f, 0.9f, 0.9f));
-		g_debugDraw.DrawPolygon(vertices, m_countA, b2Color(0.9f, 0.9f, 0.9f));
+
+		if (m_countA == 1)
+		{
+			g_debugDraw.DrawCircle(vertices[0], m_radiusA, b2Color(0.9f, 0.9f, 0.9f));
+		}
+		else
+		{
+			g_debugDraw.DrawPolygon(vertices, m_countA, b2Color(0.9f, 0.9f, 0.9f));
+		}
 
 		for (int32 i = 0; i < m_countB; ++i)
 		{
-			vertices[i] = b2Mul(transformB, m_vBs[i]);
+			vertices[i] = b2Mul(m_transformB, m_vBs[i]);
 		}
-        //g_debugDraw.DrawCircle(vertices[0], m_radiusB, b2Color(0.5f, 0.9f, 0.5f));
-        g_debugDraw.DrawPolygon(vertices, m_countB, b2Color(0.5f, 0.9f, 0.5f));
+
+		if (m_countB == 1)
+		{
+			g_debugDraw.DrawCircle(vertices[0], m_radiusB, b2Color(0.5f, 0.9f, 0.5f));
+		}
+		else
+		{
+			g_debugDraw.DrawPolygon(vertices, m_countB, b2Color(0.5f, 0.9f, 0.5f));
+		}
 
 		for (int32 i = 0; i < m_countB; ++i)
 		{
 			vertices[i] = b2Mul(transformB2, m_vBs[i]);
 		}
-        //g_debugDraw.DrawCircle(vertices[0], m_radiusB, b2Color(0.5f, 0.7f, 0.9f));
-        g_debugDraw.DrawPolygon(vertices, m_countB, b2Color(0.5f, 0.7f, 0.9f));
+
+		if (m_countB == 1)
+		{
+			g_debugDraw.DrawCircle(vertices[0], m_radiusB, b2Color(0.5f, 0.7f, 0.9f));
+		}
+		else
+		{
+			g_debugDraw.DrawPolygon(vertices, m_countB, b2Color(0.5f, 0.7f, 0.9f));
+		}
 
 		if (hit)
 		{
@@ -136,13 +177,17 @@ public:
 		}
 	}
 
-    b2Vec2 m_vAs[b2_maxPolygonVertices];
-    int32 m_countA;
-    float m_radiusA;
+	b2Vec2 m_vAs[b2_maxPolygonVertices];
+	int32 m_countA;
+	float m_radiusA;
 
-    b2Vec2 m_vBs[b2_maxPolygonVertices];
-    int32 m_countB;
-    float m_radiusB;
+	b2Vec2 m_vBs[b2_maxPolygonVertices];
+	int32 m_countB;
+	float m_radiusB;
+
+	b2Transform m_transformA;
+	b2Transform m_transformB;
+	b2Vec2 m_translationB;
 };
 
 static int testIndex = RegisterTest("Collision", "Shape Cast", ShapeCast::Create);
