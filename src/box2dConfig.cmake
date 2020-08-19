605a9acf403dfdfc52ffd5b0b15f4be9777cb259
Julien Bernard <julien.bernard@univ-fcomte.fr>
2020-08-19 18:26:00 +0200

    export to box2dConfig directly

--- src/box2dConfig.cmake
+++
@@ -1,3 +0,0 @@
-get_filename_component(BOX2D_CMAKE_DIR "${CMAKE_CURRENT_LIST_FILE}" DIRECTORY)
-include("${BOX2D_CMAKE_DIR}/box2dTargets.cmake")
-include("${BOX2D_CMAKE_DIR}/box2dConfigVersion.cmake")
