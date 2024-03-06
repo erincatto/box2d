6586a8f0c31e5c08a00ef1613341fd89ecf6c5de
Nathan Chitnis <161651939+DeveleporMaster@users.noreply.github.com>
2024-03-06 16:39:58 -0500

    turned on staticruntime

--- premake5.lua
+++ premake5.lua
@@ -2,7 +2,7 @@ project "Box2D"
         kind "StaticLib"
         language "C++"
         cppdialect "C++11"
-        staticruntime "off"
+        staticruntime "on"
 
         targetdir ("bin/" .. outputdir .. "/%{prj.name}")
         objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
