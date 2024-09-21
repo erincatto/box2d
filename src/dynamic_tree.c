3d33eecbe613c04ef613762dd23a5f33ede48e0d
Erin Catto <erincatto@users.noreply.github.com>
2024-09-21 10:43:30 -0700

    removed stretchy buffer

--- src/dynamic_tree.c
+++ src/dynamic_tree.c
@@ -1671,6 +1671,7 @@ static int32_t b2BuildTree( b2DynamicTree* tree, int32_t leafCount )
 	int32_t* binIndices = tree->binIndices;
 #endif
 
+	// todo large stack item
 	struct b2RebuildItem stack[b2_treeStackSize];
 	int32_t top = 0;
 
