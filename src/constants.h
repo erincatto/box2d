bd6824520744f1b6b000899958d016e040633202
Erin Catto <erincatto@users.noreply.github.com>
2025-04-05 17:59:35 -0700

    A user can override `B2_MAX_WORLDS`

--- src/constants.h
+++ src/constants.h
@@ -23,7 +23,9 @@ extern float b2_lengthUnitsPerMeter;
 #define B2_LINEAR_SLOP ( 0.005f * b2_lengthUnitsPerMeter )
 
 // Maximum number of simultaneous worlds that can be allocated
+#ifndef B2_MAX_WORLDS
 #define B2_MAX_WORLDS 128
+#endif
 
 // The maximum rotation of a body per time step. This limit is very large and is used
 // to prevent numerical problems. You shouldn't need to adjust this.
