b4e2298fd180163811770fce7cf9c2f04e89b830
Jenwein <rgw127310@gmail.com>
2025-08-27 12:11:13 +0800

    添加premake

---
+++ premake5.lua
@@ -0,0 +1,37 @@
+project "Box2D"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++11"
+	staticruntime "off"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"src/**.h",
+		"src/**.cpp",
+		"include/**.h"
+	}
+
+	includedirs
+	{
+		"include",
+		"src"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+
+	filter "configurations:Dist"
+		runtime "Release"
+		optimize "on"
+        symbols "off"
