ab3baa02286494d68bdca2c1706ffbbf715df5a9
Aksel Hjerpbakk <akselhallajs@gmail.com>
2025-09-12 18:46:18 +0200

    Clean up zig build and add basic CI

--- build.zig
+++ build.zig
@@ -1,7 +1,7 @@
 const std = @import("std");
 const builtin = @import("builtin");
 
-const min_supported_ver = "0.14.0";
+const min_supported_ver = "0.15.0";
 
 comptime {
     const order = std.SemanticVersion.order;
@@ -10,16 +10,61 @@ comptime {
         @compileError("Box2d requires zig version " ++ min_supported_ver);
 }
 
-fn compileBox2d(b: *std.Build, target: std.Build.ResolvedTarget, optimize: std.builtin.OptimizeMode, options: Options) !*std.Build.Step.Compile {
+pub const Options = struct {
+    shared: bool,
+    unit_tests: bool,
+
+    const defaults = Options{
+        .shared = false,
+        .unit_tests = false,
+    };
+
+    pub fn getOptions(b: *std.Build) Options {
+        return .{
+            .shared = b.option(bool, "shared", "Compile as shared library") orelse defaults.shared,
+            .unit_tests = b.option(bool, "unit_tests", "Compile units tests") orelse defaults.unit_tests,
+        };
+    }
+};
+
+pub fn build(b: *std.Build) !void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+    const options = Options.getOptions(b);
+    const lib = try compileBox2d(b, target, optimize, options.shared);
+
+    b.installArtifact(lib);
+
+    var shared_lib: ?*std.Build.Step.Compile = null;
+    if (options.unit_tests) {
+        shared_lib = buildShared(b, target, optimize, lib);
+    }
+
+    if (options.unit_tests) {
+        // link with enkiTS c headers
+        const enki_ts = get_enki_ts_artifact: {
+            const enki_ts_dep = b.dependency("enkiTS", .{
+                .shared = false,
+            });
+
+            break :get_enki_ts_artifact enki_ts_dep.artifact("enki_ts");
+        };
+
+        buildTests(b, target, optimize, lib, enki_ts, shared_lib.?);
+    }
+}
+
+fn compileBox2d(b: *std.Build, target: std.Build.ResolvedTarget, optimize: std.builtin.OptimizeMode, shared: bool) !*std.Build.Step.Compile {
     var box2d_flags_arr = std.ArrayList([]const u8).empty;
     defer box2d_flags_arr.deinit(b.allocator);
 
     try box2d_flags_arr.appendSlice(b.allocator, &[_][]const u8{
         "-std=gnu99",
         "-D_GNU_SOURCE",
+        "-ffp-contract=off",
     });
 
-    if (options.shared) {
+    if (shared) {
         try box2d_flags_arr.appendSlice(b.allocator, &[_][]const u8{
             "-fPIC",
             "-DBUILD_LIBTYPE_SHARED",
@@ -29,16 +74,15 @@ fn compileBox2d(b: *std.Build, target: std.Build.ResolvedTarget, optimize: std.b
     const module = b.addModule("box2d", .{
         .target = target,
         .optimize = optimize,
+        .link_libc = true,
     });
 
-    const linkage: std.builtin.LinkMode = if (options.shared) .dynamic else .static;
+    const linkage: std.builtin.LinkMode = if (shared) .dynamic else .static;
     const box2d = b.addLibrary(.{
         .root_module = module,
         .name = "box2d",
         .linkage = linkage,
     });
-
-    box2d.linkLibC();
 
     const c_source_files = &[_][]const u8{
         "src/aabb.c",
@@ -99,38 +143,99 @@ fn compileBox2d(b: *std.Build, target: std.Build.ResolvedTarget, optimize: std.b
         "src/wheel_joint.c",
     };
 
-    box2d.addIncludePath(b.path("include"));
+    box2d.root_module.addIncludePath(b.path("include"));
+    box2d.installHeadersDirectory(b.path("include/box2d"), "box2d", .{});
 
     box2d.root_module.addCSourceFiles(.{
         .files = c_source_files,
         .flags = box2d_flags_arr.items,
+        .language = .c,
     });
 
     return box2d;
 }
 
-pub fn build(b: *std.Build) !void {
-    const target = b.standardTargetOptions(.{});
-    const optimize = b.standardOptimizeOption(.{});
-    const lib = try compileBox2d(b, target, optimize, Options.getOptions(b));
-    lib.installHeader(b.path("include/box2d/base.h"), "base.h");
-    lib.installHeader(b.path("include/box2d/box2d.h"), "box2d/box2d.h");
-    lib.installHeader(b.path("include/box2d/collision.h"), "collision.h");
-    lib.installHeader(b.path("include/box2d/id.h"), "id.h");
-    lib.installHeader(b.path("include/box2d/math_functions.h"), "math_functions.h");
-    lib.installHeader(b.path("include/box2d/types.h"), "types.h");
+pub fn buildTests(
+    b: *std.Build,
+    target: std.Build.ResolvedTarget,
+    optimize: std.builtin.OptimizeMode,
+    box2d_lib: *std.Build.Step.Compile,
+    enki_ts_lib: *std.Build.Step.Compile,
+    box2d_shared_lib: *std.Build.Step.Compile,
+) void {
+    const module = b.createModule(.{
+        .target = target,
+        .optimize = optimize,
+    });
 
-    b.installArtifact(lib);
+    module.addCSourceFiles(.{
+        .files = &[_][]const u8{
+            "test/main.c",
+            "test/test_bitset.c",
+            "test/test_collision.c",
+            "test/test_determinism.c",
+            "test/test_distance.c",
+            "test/test_id.c",
+            "test/test_math.c",
+            "test/test_shape.c",
+            "test/test_table.c",
+            "test/test_world.c",
+        },
+        .flags = &[_][]const u8{
+            "-std=c17",
+            "-ffp-contract=off",
+        },
+        .language = .c,
+    });
+
+    const exe = b.addExecutable(.{
+        .name = "test",
+        .root_module = module,
+    });
+
+    exe.root_module.addIncludePath(b.path("src"));
+    exe.root_module.addIncludePath(b.path("shared"));
+
+    exe.root_module.linkLibrary(box2d_lib);
+    exe.root_module.linkLibrary(box2d_shared_lib);
+    exe.root_module.linkLibrary(enki_ts_lib);
+
+    b.installArtifact(exe);
 }
 
-pub const Options = struct {
-    shared: bool = false,
+pub fn buildShared(
+    b: *std.Build,
+    target: std.Build.ResolvedTarget,
+    optimize: std.builtin.OptimizeMode,
+    box2d_lib: *std.Build.Step.Compile,
+) *std.Build.Step.Compile {
+    const module = b.createModule(.{
+        .target = target,
+        .optimize = optimize,
+    });
 
-    const defaults = Options{};
+    module.addCSourceFiles(.{
+        .files = &[_][]const u8{
+            "shared/benchmarks.c",
+            "shared/benchmarks.h",
+            "shared/determinism.c",
+            "shared/determinism.h",
+            "shared/human.c",
+            "shared/human.h",
+            "shared/random.c",
+            "shared/random.h",
+        },
+        .flags = &[_][]const u8{
+            "-ffp-contract=off",
+        },
+        .language = .c,
+    });
 
-    pub fn getOptions(b: *std.Build) Options {
-        return .{
-            .shared = b.option(bool, "shared", "Compile as shared library") orelse defaults.shared,
-        };
-    }
-};
+    module.linkLibrary(box2d_lib);
+
+    return b.addLibrary(.{
+        .root_module = module,
+        .name = "shared",
+        .linkage = .static,
+    });
+}
