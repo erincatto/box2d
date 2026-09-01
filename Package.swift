52a6d1655e4576ceb614b76ff96fb7bfbca25e94
Erin Catto <erincatto@users.noreply.github.com>
2026-09-01 13:43:08 -0700

    swift support

---
+++ Package.swift
@@ -0,0 +1,36 @@
+// swift-tools-version:5.10
+import PackageDescription
+
+let package = Package(
+    name: "Box2D",
+    products: [
+        .library(
+            name: "Box2D",
+            targets: ["Box2D"]
+        ),
+    ],
+    targets: [
+        .target(
+            name: "Box2D",
+            path: ".",
+            exclude: [
+                "src/box2d.natvis",
+                "src/CMakeLists.txt"
+            ],
+            sources: [
+                "src"
+            ],
+            publicHeadersPath: "include",
+            cSettings: [
+                .headerSearchPath("include"),
+                .headerSearchPath("src")
+            ]
+        ),
+        .testTarget(
+            name: "test_world",
+            dependencies: ["Box2D"],
+            path: "test/swift"
+        ),
+    ],
+    cLanguageStandard: .c17
+)
---
+++ test/swift/test_world.swift
@@ -0,0 +1,39 @@
+import XCTest
+import Box2D
+
+final class Box2DTests: XCTestCase {
+    func testVersion() {
+        let version = b2GetVersion()
+        XCTAssertGreaterThanOrEqual(version.major, 3)
+    }
+
+    func testWorldStep() {
+        var worldDef = b2DefaultWorldDef()
+        let worldId = b2CreateWorld(&worldDef)
+        XCTAssertTrue(b2World_IsValid(worldId))
+
+        var groundDef = b2DefaultBodyDef()
+        groundDef.position = b2Pos(x: 0.0, y: -10.0)
+        let groundId = b2CreateBody(worldId, &groundDef)
+        var groundBox = b2MakeBox(100.0, 1.0)
+        var shapeDef = b2DefaultShapeDef()
+        _ = b2CreatePolygonShape(groundId, &shapeDef, &groundBox)
+
+        var bodyDef = b2DefaultBodyDef()
+        bodyDef.position = b2Pos(x: 0.0, y: 10.0)
+        let bodyId = b2CreateBody(worldId, &bodyDef)
+        var bodyBox = b2MakeBox(0.5, 0.5)
+        _ = b2CreatePolygonShape(bodyId, &shapeDef, &bodyBox)
+
+        for _ in 0..<120 {
+            b2World_Step(worldId, 1.0 / 60.0, 4)
+        }
+
+        let y = b2Body_GetPosition(bodyId).y
+        XCTAssertLessThan(y, 10.0)
+        XCTAssertGreaterThan(y, -10.0)
+
+        b2DestroyWorld(worldId)
+        XCTAssertFalse(b2World_IsValid(worldId))
+    }
+}
