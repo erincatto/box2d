1ebe0011bc3322bd695589c58844d1ec504d4d5b
Erin Catto <erincatto@users.noreply.github.com>
2026-09-04 12:36:32 -0700

    CI fix

--- src/dynamic_tree.c
+++ src/dynamic_tree.c
@@ -4,9 +4,9 @@
 #include "aabb.h"
 #include "atomic.h"
 #include "core.h"
+#include "dynamic_tree.h"
 
 #include "box2d/collision.h"
-#include "box2d/constants.h"
 #include "box2d/math_functions.h"
 
 #include <float.h>
